0cda135ae881b8947f95cdb7e4e7d4f1e3c3fa26
Pol Gómez Guerrero <thebestpol@gmail.com>
2017-08-04 22:36:42 +0200

    Created activity dependencies retention policy annotation

--- app/src/main/java/com/socialsearch/core/di/PerActivity.java
+++ app/src/main/java/com/socialsearch/core/di/PerActivity.java
@@ -1,10 +1,15 @@
 package com.socialsearch.core.di;
 
+import java.lang.annotation.Retention;
+import javax.inject.Scope;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 /**
  * SocialSearchDemo
  * com.socialsearch.core.di
  * PerActivity
  */
 
-public @interface PerActivity {
+@Scope @Retention(RUNTIME) public @interface PerActivity {
 }
