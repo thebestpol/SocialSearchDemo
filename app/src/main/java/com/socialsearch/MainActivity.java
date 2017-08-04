ff778563e7497da98483ca3d3a0458634fa1cb2b
Pol Gómez Guerrero <thebestpol@gmail.com>
2017-08-04 22:15:22 +0200

    MainActivity should be a fragment container

--- app/src/main/java/com/socialsearch/MainActivity.java
+++ app/src/main/java/com/socialsearch/MainActivity.java
@@ -1,5 +1,6 @@
 package com.socialsearch;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -8,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    getFragmentManager().beginTransaction().add(R.id.container, new Fragment()).commit();
   }
 }
---
+++ app/src/test/java/com/socialsearch/MainActivityShould.java
@@ -0,0 +1,40 @@
+package com.socialsearch;
+
+import android.app.FragmentManager;
+import android.view.View;
+import com.socialsearch.rules.RobolectricMockApplicationComponentRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.robolectric.Shadows.shadowOf;
+
+/**
+ * SocialSearchDemo
+ * com.socialsearch
+ * MainActivityShould
+ */
+@RunWith(RobolectricTestRunner.class) public class MainActivityShould {
+
+  @Rule public RobolectricMockApplicationComponentRule rule =
+      new RobolectricMockApplicationComponentRule();
+
+  @Test public void contain_fragment_container() {
+    MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+
+    View container = shadowOf(mainActivity).findViewById(R.id.container);
+    assertThat(container, notNullValue());
+  }
+
+  @Test public void contain_search_fragment_on_start() {
+    MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+
+    FragmentManager fragmentManager = mainActivity.getFragmentManager();
+
+    assertThat(fragmentManager.findFragmentById(R.id.container), notNullValue());
+  }
+}
