d3dbc4b74ccf5faa2d4daad381e4394c5b0b5591
Pol Gómez Guerrero <thebestpol@gmail.com>
2017-08-04 22:22:16 +0200

    Activity should initialize user story

---
+++ app/src/main/java/com/socialsearch/main/DemoUserStory.java
@@ -0,0 +1,18 @@
+package com.socialsearch.main;
+
+/**
+ * SocialSearchDemo
+ * com.socialsearch.main
+ * DemoUserStory
+ */
+
+public class DemoUserStory {
+
+  public DemoUserStory() {
+
+  }
+
+  public void initialize() {
+
+  }
+}
--- app/src/test/java/com/socialsearch/MainActivityShould.java
+++ app/src/test/java/com/socialsearch/MainActivityShould.java
@@ -2,15 +2,18 @@ package com.socialsearch;
 
 import android.app.FragmentManager;
 import android.view.View;
+import com.socialsearch.main.DemoUserStory;
 import com.socialsearch.rules.RobolectricMockApplicationComponentRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.robolectric.Shadows.shadowOf;
 
 /**
@@ -22,6 +25,8 @@ import static org.robolectric.Shadows.shadowOf;
 
   @Rule public RobolectricMockApplicationComponentRule rule =
       new RobolectricMockApplicationComponentRule();
+
+  @Mock DemoUserStory mockUserStory;
 
   @Test public void contain_fragment_container() {
     MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
@@ -36,5 +41,11 @@ import static org.robolectric.Shadows.shadowOf;
     FragmentManager fragmentManager = mainActivity.getFragmentManager();
 
     assertThat(fragmentManager.findFragmentById(R.id.container), notNullValue());
+  }
+
+  @Test public void initialize_user_story() {
+    MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+
+    verify(mockUserStory).initialize();
   }
 }
