8ad6de12f4335d4685ef9c5194c9b6e0344abd35
Pol Gómez Guerrero <thebestpol@gmail.com>
2017-08-05 20:49:07 +0200

    Green lights

--- app/src/main/java/com/socialsearch/history/presenter/HistoryPresenter.java
+++ app/src/main/java/com/socialsearch/history/presenter/HistoryPresenter.java
@@ -1,8 +1,11 @@
 package com.socialsearch.history.presenter;
 
+import com.socialsearch.core.model.Callback;
 import com.socialsearch.core.presenter.Presenter;
+import com.socialsearch.entity.HistoryData;
 import com.socialsearch.history.model.HistoryModel;
 import com.socialsearch.history.view.HistoryView;
+import java.util.List;
 
 /**
  * SocialSearchDemo
@@ -12,10 +15,22 @@ import com.socialsearch.history.view.HistoryView;
 
 public class HistoryPresenter extends Presenter<HistoryView> {
 
+  private final HistoryModel model;
+
   public HistoryPresenter(HistoryModel model) {
+    this.model = model;
   }
 
   @Override public void start() {
+    model.obtainHistoryData(new Callback<List<HistoryData>>() {
+      @Override public void onSuccess(List<HistoryData> response) {
+        view.loadHistoryData(response);
+      }
+
+      @Override public void onError(String errorMessage) {
+
+      }
+    });
   }
 
   @Override public void stop() {
