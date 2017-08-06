480b1f97609269b08d41bdb358eddd4e71216d41
Pol Gómez Guerrero <thebestpol@gmail.com>
2017-08-06 19:50:02 +0200

    Starting repository

---
+++ app/src/main/java/com/socialsearch/data/SocialDataRepository.java
@@ -0,0 +1,21 @@
+package com.socialsearch.data;
+
+import com.socialsearch.entity.SocialData;
+import rx.Observable;
+
+/**
+ * SocialSearchDemo
+ * com.socialsearch.data
+ * SocialDataRepository
+ */
+
+public class SocialDataRepository {
+
+  public Observable<SocialData> getTweetsSocialData(String query) {
+    return null;
+  }
+
+  public Observable<SocialData> getPlusSocialData(String query) {
+    return null;
+  }
+}
---
+++ app/src/test/java/com/socialsearch/data/SocialDataRepositoryShould.java
@@ -0,0 +1,15 @@
+package com.socialsearch.data;
+
+import org.junit.Test;
+
+/**
+ * SocialSearchDemo
+ * com.socialsearch.data
+ * SocialDataRepositoryShould
+ */
+public class SocialDataRepositoryShould {
+
+  @Test public void get_tweets_from_data_source_and_map_to_social_data() {
+
+  }
+}
